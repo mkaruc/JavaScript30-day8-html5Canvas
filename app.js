d4e75fa72884fb8737069ad456d743e429e16d0a
mkaruc <fatihmehmetkaruc@gmail.com>
2022-06-17 01:48:06 +0300

    onGoing

--- app.js
+++ app.js
@@ -4,16 +4,36 @@ canvas.width=window.innerWidth;
 canvas.height=window.innerHeight;
 ctx.strokeStyle="#BADA55";
 ctx.lineJoin="round"; 
-ctx.lineCap="round"; 
+ctx.lineCap="round";
+ctx.lineWidth=100;
 let isDrawing=false;
 let lastX=0;
 let lastY=0;
+let hue=0;
+let direction=true;
 function draw(e){
     if(!isDrawing) return //stop drawing when mouse doesn't move
     console.log(e);
     ctx.beginPath();
+    ctx.moveTo(lastX,lastY)//starting..
+    ctx.lineTo(e.offsetX,e.offsetY);
+    ctx.stroke();
+    [lastX,lastY]=[e.offsetX,e.offsetY];
+    hue++;
+    if(hue>=300){
+        hue=0;
+    }
+    if(ctx.lineWidth<=1||ctx.lineWidth>=100){
+        direction= !direction;
+    }
+    if (direction) {
+        ctx.lineWidth++;
+    }else{
+        ctx.lineWidth--;
+    }
 }
 canvas.addEventListener('mousemove',draw);
-canvas.addEventListener('mousedown',()=> isDrawing=true);
+    
+    canvas.addEventListener('mousedown',(e)=>{ isDrawing=true; [lastX,lastY]=[e.offsetX,e.offsetY]; });
 canvas.addEventListener('mouseup',()=> isDrawing=false);
 canvas.addEventListener('mouseout',()=> isDrawing=false);
