a653eb2c7d447353b7d4a1f1b8e4c9e3d4f90fb7
mkaruc <fatihmehmetkaruc@gmail.com>
2022-06-16 23:44:50 +0300

    canvasStarted

---
+++ app.js
@@ -0,0 +1,19 @@
+const canvas=document.querySelector("#draw");
+const ctx=canvas.getContext("2d");
+canvas.width=window.innerWidth;
+canvas.height=window.innerHeight;
+ctx.strokeStyle="#BADA55";
+ctx.lineJoin="round"; 
+ctx.lineCap="round"; 
+let isDrawing=false;
+let lastX=0;
+let lastY=0;
+function draw(e){
+    if(!isDrawing) return //stop drawing when mouse doesn't move
+    console.log(e);
+    ctx.beginPath();
+}
+canvas.addEventListener('mousemove',draw);
+canvas.addEventListener('mousedown',()=> isDrawing=true);
+canvas.addEventListener('mouseup',()=> isDrawing=false);
+canvas.addEventListener('mouseout',()=> isDrawing=false);
